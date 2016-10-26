1351809c594a16b6ccc39596a3b06124d6cf7745
Saverio <s.meucci91@gmail.com>
2016-10-26 15:47:21 +0200

    added new option merge to cmdline

--- src/Menu.java
+++ src/Menu.java
@@ -4,24 +4,30 @@ import static tool.VFT.*;
 import org.apache.commons.cli.*;
 
 public class Menu {
-
-	public static String input;
-	public static String output;
 	
 	public static void main(String args[]) throws Exception {
 		
 		CommandLine cmd = parseArguments(args);
 		if (cmd.hasOption("parse")) {
-			input = cmd.getOptionValue("input");
-			output = cmd.getOptionValue("output");
+			String input = cmd.getOptionValue("input");
+			String output = cmd.getOptionValue("output");
 			parse(input, output);
 		} else if (cmd.hasOption("draw")) {
-			input = cmd.getOptionValue("input");
+			String input = cmd.getOptionValue("input");
 			draw(input, "Tree of: " + input + " - Press 0 to exit.");
 		} else if (cmd.hasOption("batch")) {
-			input = cmd.getOptionValue("input");
-			output = cmd.getOptionValue("output");
+			String input = cmd.getOptionValue("input");
+			String output = cmd.getOptionValue("output");
 			batch(input, output);
+		} else if (cmd.hasOption("merge")) {
+			String input1 = cmd.getOptionValue("input");			
+			String input2 = cmd.getOptionValue("input2");
+			String output = cmd.getOptionValue("output");
+			if (cmd.hasOption("withAttributes")) {
+				merge(input1, input2, output, true);
+			} else {
+				merge(input1, input2, output, false);
+			}
 		}
 	}
 	
@@ -33,6 +39,7 @@ public class Menu {
 		group.addOption(new Option("d", "draw", false, "draw a tree from an xml file"));
 		group.addOption(new Option("p", "parse", false, "parse a video file container into a xml file"));
 		group.addOption(new Option("b", "batch", false, "batch parse a directory of video files; it recreates the same folder structure"));
+		group.addOption(new Option("m", "merge", false, "merge two xml file into one"));
 		group.addOption(new Option("h", "help", false, "print help message"));
 		opts.addOptionGroup(group);
 		
@@ -44,6 +51,14 @@ public class Menu {
 				.build();
 		opts.addOption(input_opt);
 		
+		Option input_opt2 = Option.builder("i2")
+				.longOpt("input2")
+				.argName("file")
+				.hasArg()
+				.desc("second xml file for --merge")
+				.build();
+		opts.addOption(input_opt2);
+		
 		Option output_opt = Option.builder("o")
 				.longOpt("output")
 				.argName("folder")
@@ -51,6 +66,13 @@ public class Menu {
 				.desc("output folder for the xml file, only for --parse")
 				.build();
 		opts.addOption(output_opt);
+		
+		Option withAttributes_opt = Option.builder("wa")
+				.longOpt("withAttributes")
+				.argName("boolean")
+				.desc("whether to consider attributes in --merge or not. Default is false")
+				.build();
+		opts.addOption(withAttributes_opt);
 		
 		CommandLine cl = null;
 		HelpFormatter formatter = new HelpFormatter();
@@ -78,6 +100,10 @@ public class Menu {
 	        System.exit(0);
 	    } else if (cl.hasOption("b") && (!cl.hasOption("i") || !cl.hasOption("o"))) {
 	    	System.err.println("Missing options: [i | o]");
+	    	formatter.printHelp("vft", opts, true);
+	        System.exit(0);
+	    } else if (cl.hasOption("m") && (!cl.hasOption("i") ||!cl.hasOption("i2"))) {
+	    	System.err.println("Missing options: [i | i2]");
 	    	formatter.printHelp("vft", opts, true);
 	        System.exit(0);
 	    }
